c4a5a88feb534f5ce2c749e5686e19b2e5ce80b2
Hapi-W <spendasbjm@gmail.com>
2025-05-12 16:20:14 +0800

    initial commit

--- app/src/main/java/com/example/amazonapp/PilihNegara.java
+++ app/src/main/java/com/example/amazonapp/PilihNegara.java
@@ -10,5 +10,6 @@ public class PilihNegara extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pilih_negara);
+
     }
 }
