fa56b2c73ac1ebf5797fed28a2c03504558148bd
Hapi-W <spendasbjm@gmail.com>
2025-05-12 17:21:17 +0800

    initial commit

--- app/src/main/java/com/example/amazonapp/already_costumer_login.java
+++ app/src/main/java/com/example/amazonapp/already_costumer_login.java
@@ -12,7 +12,5 @@ public class already_costumer_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_already_costumer_login);
-
-
     }
 }
