356920676473818779930ebfaba30ab08be64dba
Hapi-W <spendasbjm@gmail.com>
2025-05-12 10:12:29 +0800

    initial commit

--- app/src/main/java/com/example/amazonapp/already_costumer_login.java
+++ app/src/main/java/com/example/amazonapp/already_costumer_login.java
@@ -2,7 +2,9 @@ package com.example.amazonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class already_costumer_login extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class already_costumer_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_already_costumer_login);
+
+
     }
 }
