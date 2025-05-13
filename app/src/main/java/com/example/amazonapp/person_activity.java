a22e07aa007181b92eb588ea59edf4dfe641276f
Hapi-W <spendasbjm@gmail.com>
2025-05-13 22:02:09 +0800

    initial commit

---
+++ app/src/main/java/com/example/amazonapp/person_activity.java
@@ -0,0 +1,14 @@
+package com.example.amazonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class person_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person);
+    }
+}
