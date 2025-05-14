cd0b277a045e902f4e280551e56fa5e7d88dd433
AdhimMaulana <adhimsmk2bjm@gmail.com>
2025-05-14 14:22:55 +0800

    initial commit

--- app/src/main/java/com/example/amazonapp/DetailItem.java
+++ app/src/main/java/com/example/amazonapp/DetailItem.java
@@ -1,8 +1,15 @@
 package com.example.amazonapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class DetailItem extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class DetailItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_item);
+
+        BottomNavigationView bot_nav = findViewById(R.id.bottomNavigationView);
+        bot_nav.setSelectedItemId(R.id.home);
+
+        bot_nav.setOnNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                startActivity(new Intent(DetailItem.this, Home.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.person) {
+                return true;
+            } else if (item.getItemId() == R.id.cart) {
+                startActivity(new Intent(DetailItem.this, Cart_Activity.class));
+                finish();
+                return true;
+            } else {
+                startActivity(new Intent(DetailItem.this, Setting.class));
+                finish();
+                return true;
+            }
+        });
+    }
+
+    public void toastAdd(View v) {
+        Toast.makeText(DetailItem.this, "Telah Ditambahkan", Toast.LENGTH_SHORT).show();
     }
 }
